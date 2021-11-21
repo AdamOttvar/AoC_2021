a743363c25fc055bdf707ac188a8c760457bebfc
Adam Ottvar <adam.ottvar@gmail.com>
2021-11-21 21:12:53 +0100

    Create dummy files and "boilerplate"

---
+++ dummy.py
@@ -0,0 +1,19 @@
+
+def first_part(input_file):
+    result = 1
+
+    print(result)
+
+def second_part(input_file):
+    result = 2
+
+    print(result)
+
+
+if __name__ == '__main__':
+    # Read in the input file.
+    with open("inputs/dummy.txt", "r") as myfile:
+        input_file = [i.rstrip("\n") for i in myfile.readlines()]
+    
+    first_part(input_file)
+    second_part(input_file)
